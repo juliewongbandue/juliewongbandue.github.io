6f22897d60664fd7577061198eda747525e7ce30
juliewongbandue <juliewongbandue@julies-MacBook-Pro.local>
2016-11-02 09:39:31 -0400

    jquery

--- homework/lifetime-supply/js/project.js
+++ homework/lifetime-supply/js/project.js
@@ -7,17 +7,25 @@
 //6. Output your results to the user
 
 
-document.getElementById('click-me').onclick = calculate;
+document.getElementById('click-me').onclick = calculate; //define the event
+(onlick)
 
 function calculate (){
+	//get inputs (creating variables), make sure you remember to make sure
+	//it's not a string by using "parseINT"
 	var age = parseInt(document.getElementById('age').value);
 	var maxAge = parseInt(document.getElementById('max-age').value);
 	var favoriteDrink = document.getElementById('item').value;
 	var numPerDay = parseInt(document.getElementById('num-per-day').value);
 	
+	
+	console.log (age, maxAge, favDrink, numPerDay)
+	//for debugging: if you want to know what type of data you're working with
+
 	var solution = (maxAge-age) * numPerDay * 365;
+	//MATH!!!!
 
 	document.getElementById('solution').innerHTML = solution
 	document.getElementById('drink').innerHTML = favoriteDrink
-	
+	//output: put the results in the HTML so sentences make sense.
 }
