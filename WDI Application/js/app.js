f6956d290ca5a62c77ade1295e9bb81342e3f034
juliewongbandue <juliewongbandue@julies-MacBook-Pro.local>
2017-04-07 13:52:02 -0400

    typed

--- WDI Application/js/app.js
+++ WDI Application/js/app.js
@@ -1,6 +1,6 @@
 $(function(){
 		$(".adjectives").typed({
-			strings: ["super cool", "witty", "faking it til I make it", "a lover of the interwebs", "a creative", "a painter", "a wanna-be designer", "a future programmer..."],
+			strings: ["super cool ^200", "pretty cool ^300", "witty ^200", "faking it til I make it ^1000", "a lover ^1000of the interwebs  ^1000", "a creative ^300", "a painter ^300", "a wanna-be designer^500", "a future programmer..."],
 			typeSpeed: 0
 		});
 	});
