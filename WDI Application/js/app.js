7e0e1d1a1ddaa48bec17bc0d68645c2062474ece
juliewongbandue <juliewongbandue@julies-MacBook-Pro.local>
2017-05-27 15:01:08 -0400

    stuff

--- WDI Application/js/app.js
+++ WDI Application/js/app.js
@@ -1,6 +1,6 @@
-$(function(){
-		$(".adjectives").typed({
-			strings: ["super cool ^200", "pretty cool ^300", "witty ^200", "faking it til I make it ^1000", "a lover ^1000of the interwebs  ^1000", "a creative ^300", "a painter ^300", "a wanna-be designer ^500", "a future programmer^500.^500.^500."],
-			typeSpeed: 0
-		});
-	});
+$(function() {
+    $(".adjectives").typed({
+        strings: ["super cool ^200", "pretty cool ^300", "witty ^200", "faking it til I make it ^1000", "a lover ^1000of the interwebs  ^1000", "a creative ^300", "a painter ^300", "a wanna-be designer ^500", "a future programmer^500.^500.^500."],
+        typeSpeed: 0
+    });
+});
