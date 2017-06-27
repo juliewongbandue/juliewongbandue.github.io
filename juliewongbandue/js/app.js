55f4db4131b2404424bf4e8334cf580c593e8963
Julie Wongbandue <juliewongbandue@gmail.com>
2017-06-27 00:02:25 -0400

    updates

--- juliewongbandue/js/app.js
+++ juliewongbandue/js/app.js
@@ -2,7 +2,7 @@ $(function() {
   var $adj = $(".adjectives")
   console.log ($adj)
     $adj.typed({
-        strings: ["super cool ^200", "pretty cool ^300", "witty ^200", "faking it til I make it ^1000", "a lover ^1000of the interwebs  ^1000", "a creative ^300", "a painter ^300", "a wanna-be designer ^500", "a future web developer^500.^500.^500."],
+        strings: ["super cool ^200", "pretty cool ^300", "witty ^200", "faking it til I make it ^1000", "a lover ^1000of the interwebs  ^1000", "a creative ^300", "a painter ^300", "a wanna-be designer ^500", "a web developer."],
         typeSpeed: 0
     });
 });
