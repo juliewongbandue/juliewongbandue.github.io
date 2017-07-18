d308c37a9fd3fd2629698c711f0f3a9c26ffeaaf
Julie Wongbandue <juliewongbandue@gmail.com>
2017-07-18 10:18:03 -0400

    portfolio updates

--- juliewongbandue/js/app.js
+++ juliewongbandue/js/app.js
@@ -1,8 +1,26 @@
 $(function() {
+  console.log("jquery live")
   var $adj = $(".adjectives")
-  console.log ($adj)
     $adj.typed({
-        strings: ["super cool ^200", "pretty cool ^300", "witty ^200", "faking it til I make it ^1000", "a lover ^1000of the interwebs  ^1000", "a creative ^300", "a painter ^300", "a wanna-be designer ^500", "a web developer."],
+        strings: ["super cool ^200", "kinda cool ^300", "a creative ^300", "a painter ^300", "a wanna-be designer ^500", "a web developer.", ],
         typeSpeed: 0
     });
+
+    // $(".frontEndDiv").mouseover(e => {
+    //   console.log("i was hovered!");
+    //   $(".backEndDiv").flip();
+    // });
+    // $(".frontEndDiv").mouseout(e => {
+    //   console.log("unhovered")
+    //   $(".backEndDiv").flip(true);
+    // });
+
+    $(".frontEndDiv").flip({
+      trigger: 'hover'
+    })
+
+    $(".backEndDiv").flip({
+      trigger: 'hover'
+    })
+
 });
