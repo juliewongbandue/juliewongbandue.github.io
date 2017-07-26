480763647aef0be24ab17815e22357f2c0eca231
Julie Wongbandue <juliewongbandue@gmail.com>
2017-07-25 23:43:24 -0400

    images and stuff

--- juliewongbandue/js/app.js
+++ juliewongbandue/js/app.js
@@ -1,26 +1,34 @@
 $(function() {
-  console.log("jquery live")
-  var $adj = $(".adjectives")
-    $adj.typed({
-        strings: ["super cool ^200", "kinda cool ^300", "a creative ^300", "a painter ^300", "a wanna-be designer ^500", "a web developer.", ],
-        typeSpeed: 0
-    });
+  console.log("jquery live");
+  var $adj = $(".adjectives");
+  $adj.typed({
+    strings: [
+      "super cool ^200",
+      "kinda cool ^300",
+      "a creative ^300",
+      "a painter ^300",
+      "a wanna-be designer ^500",
+      "a web developer."
+    ],
+    typeSpeed: 0
+  });
 
-    // $(".frontEndDiv").mouseover(e => {
-    //   console.log("i was hovered!");
-    //   $(".backEndDiv").flip();
-    // });
-    // $(".frontEndDiv").mouseout(e => {
-    //   console.log("unhovered")
-    //   $(".backEndDiv").flip(true);
-    // });
+  // $(".frontEndDiv").mouseover(e => {
+  //   console.log("i was hovered!");
+  //   $(".backEndDiv").flip();
+  // });
+  // $(".frontEndDiv").mouseout(e => {
+  //   console.log("unhovered")
+  //   $(".backEndDiv").flip(true);
+  // });
 
-    $(".frontEndDiv").flip({
-      trigger: 'hover'
-    })
+  $(".frontEndDiv").flip({
+    trigger: "hover"
+  });
 
-    $(".backEndDiv").flip({
-      trigger: 'hover'
-    })
+  $(".backEndDiv").flip({
+    trigger: "hover"
+  });
+
 
 });
