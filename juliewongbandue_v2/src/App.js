b77f424caa2c8b14d97edb87e805331e0597cf5f
Julie Wongbandue <juliewongbandue@gmail.com>
2018-10-19 12:48:45 -0400

    configuring css-loader

--- juliewongbandue_v2/src/App.js
+++ juliewongbandue_v2/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Landing from './components/landing'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Landing/>
       </div>
     );
   }
---
+++ juliewongbandue_v2/src/components/landing.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import styles from './styles/landing.css'
+
+export default class Landing extends Component {
+    render(){
+        return (
+            <button className={styles.button}>Vanity Button</button>
+        )
+    }
+}
