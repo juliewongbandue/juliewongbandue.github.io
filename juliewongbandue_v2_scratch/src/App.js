8a4f1deb703ae696da3c5f9e10ac8231b75297fa
Julie Wongbandue <juliewongbandue@gmail.com>
2019-01-06 23:29:13 -0500

    setting up parent styles

--- juliewongbandue_v2_scratch/src/App.js
+++ juliewongbandue_v2_scratch/src/App.js
@@ -5,28 +5,39 @@ import Navigation from './components/navigation.js';
 import style from "./styles/Global.css";
 
 class App extends Component{
-  
+
   constructor(props){
     super(props)
     this.state = {
       section: 'home',
       height: '',
-      width: ''
+      width: '',
+      transform: ''
     }
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
   }
 
   componentDidMount() {
     this.updateWindowDimensions();
+    window.addEventListener('scroll', this.handleScroll);
     window.addEventListener('resize', this.updateWindowDimensions);
   }
   componentWillUnmount() {
     window.removeEventListener('resize', this.updateWindowDimensions);
+    window.removeEventListener('scroll', this.handleScroll);
   }
   updateWindowDimensions() {
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
-  
+  handleScroll() {
+    let scrollTop = event.srcElement.body.scrollTop,
+        itemTranslate = Math.min(0, scrollTop/3 - 60);
+
+    this.setState({
+      transform: itemTranslate
+    })
+  }  
   render(){
     console.log(this.state)
     return(
